c7e087f4949e94b9157e8dfec572a57f01e605f0
aricart <alberto@synadia.com>
2020-02-25 09:33:50 -0400

    - update dependencies - adoption of standardjs for project formatting - build streamline, update node versions

--- examples/nuid_test.js
+++ examples/nuid_test.js
@@ -1,15 +1,15 @@
 #!/usr/bin/env node
 
-'use strict';
+'use strict'
 
-const NUID = require('../');
+const NUID = require('../')
 
 // print the version
-console.log(NUID.version);
+console.log(NUID.version)
 
 // print some nuids
-console.log(NUID.next());
-console.log(NUID.next());
+console.log(NUID.next())
+console.log(NUID.next())
 // change the prefix
-NUID.reset();
-console.log(NUID.next());
+NUID.reset()
+console.log(NUID.next())
